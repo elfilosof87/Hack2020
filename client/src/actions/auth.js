8e6391b96958878b922d0fea3f73f63a55f9e71d
Maaz Ahmed <elfilosof@protonmail.com>
2020-02-09 11:08:22 +0530

    logout done, authentication done

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -6,7 +6,8 @@ import {
   USER_LOADED,
   AUTH_ERROR,
   LOGIN_FAIL,
-  LOGIN_SUCCESS
+  LOGIN_SUCCESS,
+  LOGOUT
 } from "./types";
 import setAuthToken from "../utils/setAuthToken";
 
@@ -91,4 +92,10 @@ export const login = (email, password) => async dispatch => {
       type: LOGIN_FAIL
     });
   }
+};
+
+//Logout / Clear profile
+
+export const logout = () => dispatch => {
+  dispatch({ type: LOGOUT });
 };
