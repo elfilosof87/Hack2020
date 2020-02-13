68455c3b7bc0a6074d090dc690a2bfcf9941b166
Maaz Ahmed <elfilosof@protonmail.com>
2020-02-13 08:36:37 +0530

    Display profile and action,reducer, types done

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -50,8 +50,7 @@ export default function(state = initialState, action) {
     case GET_REPOS:
       return {
         ...state,
-        profile: null,
-        repos: [],
+        repos: payload,
         loading: false
       };
     default:
